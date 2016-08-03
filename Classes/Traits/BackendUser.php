58a386f2d1009b1447e1dab6dd20efddf74eb951
Alexander Schnitzler <git@alexanderschnitzler.de>
2016-08-03 09:50:03 +0200

    [TASK] Add traits for DatabaseConnection, BackendUser and LanguageService

---
+++ Classes/Traits/BackendUser.php
@@ -0,0 +1,31 @@
+<?php
+namespace Extension\Templavoila\Traits;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Trait Extension\Templavoila\Traits\BackendUser
+ */
+trait BackendUser
+{
+
+    /**
+     * @return \TYPO3\CMS\Core\Authentication\BackendUserAuthentication
+     */
+    static public function getBackendUser()
+    {
+        return $GLOBALS['BE_USER'];
+    }
+
+}
---
+++ Classes/Traits/LanguageService.php
@@ -0,0 +1,31 @@
+<?php
+namespace Extension\Templavoila\Traits;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Trait Extension\Templavoila\Traits\LanguageService
+ */
+trait LanguageService
+{
+
+    /**
+     * @return \TYPO3\CMS\Lang\LanguageService
+     */
+    static public function getLanguageService()
+    {
+        return $GLOBALS['LANG'];
+    }
+
+}
