cd4999097499912262e45927b11f5c61b7a07cfd
Alexander Schnitzler <git@alexanderschnitzler.de>
2016-10-20 10:31:04 +0200

    [TASK] Update min and max php version compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-5.6.99',
+            'php' => '5.5.0-7.0.99',
             'typo3' => '7.0.0-7.9.99',
             'static_info_tables' => '',
         ],
