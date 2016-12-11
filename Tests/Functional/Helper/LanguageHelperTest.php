a0077f8d694f7f4cfb80152550b9939d2d569af4
Alexander Schnitzler <git@alexanderschnitzler.de>
2016-12-11 15:57:52 +0100

    [TASK] Add functional tests for LanguageHelper

---
+++ Tests/Functional/Helper/LanguageHelperTest.php
@@ -0,0 +1,424 @@
+<?php
+
+namespace Schnitzler\Templavoila\Tests\Functional\Service;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use Schnitzler\Templavoila\Helper\LanguagesHelper;
+use TYPO3\CMS\Core\Core\Bootstrap;
+use TYPO3\CMS\Core\Tests\FunctionalTestCase;
+
+/**
+ * Class Schnitzler\Templavoila\Tests\Functional\Service\LanguageHelperTest
+ */
+class LanguageHelperTest extends FunctionalTestCase
+{
+
+    /**
+     * @var array
+     */
+    protected $testExtensionsToLoad = [
+        'typo3/sysext/version',
+        'typo3/sysext/workspaces',
+        'typo3conf/ext/templavoila'
+    ];
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->backendUserFixture = 'typo3conf/ext/templavoila/Tests/Functional/Helper/LanguageHelperTestFixtures/be_users.xml';
+
+        $fixtureTables = [
+            'pages',
+            'pages_language_overlay',
+            'sys_language'
+        ];
+
+        $fixtureRootPath = ORIGINAL_ROOT . 'typo3conf/ext/templavoila/Tests/Functional/Helper/LanguageHelperTestFixtures/';
+
+        foreach ($fixtureTables as $table) {
+            $this->getDatabaseConnection()->exec_TRUNCATEquery($table);
+            $this->importDataSet($fixtureRootPath . $table . '.xml');
+        }
+
+        Bootstrap::getInstance()->initializeLanguageObject();
+    }
+
+    public function testGetAllWithoutTypoScriptConfig()
+    {
+        $this->setUpBackendUserFromFixture(1);
+
+        $expected = [
+            -1 => [
+                'uid' => '-1',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'All',
+                'flag' => 'multiple',
+                'language_isocode' => 'DEF',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => 'flags-multiple'
+            ],
+            0 => [
+                'uid' => '0',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'Default',
+                'flag' => '',
+                'language_isocode' => 'DEF',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => ''
+            ],
+            1 => [
+                'uid' => '1',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'German',
+                'flag' => 'de',
+                'language_isocode' => 'de',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => 'flags-de'
+            ],
+            2 => [
+                'uid' => '2',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'French',
+                'flag' => 'fr',
+                'language_isocode' => 'fr',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => 'flags-fr'
+            ],
+            3 => [
+                'uid' => '3',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'Chinese',
+                'flag' => 'cn',
+                'language_isocode' => 'zh',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => 'flags-cn'
+            ]
+        ];
+
+        static::assertSame($expected, LanguagesHelper::getAll(0));
+    }
+
+    public function testGetAllWithDefaultLanguageTypoScriptConfig()
+    {
+        $this->setUpBackendUserFromFixture(1);
+
+        $expected = [
+            -1 => [
+                'uid' => '-1',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'All',
+                'flag' => 'multiple',
+                'language_isocode' => 'DEF',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => 'flags-multiple'
+            ],
+            0 => [
+                'uid' => '0',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'English',
+                'flag' => 'gb',
+                'language_isocode' => 'DEF',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => 'flags-gb'
+            ],
+            1 => [
+                'uid' => '1',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'German',
+                'flag' => 'de',
+                'language_isocode' => 'de',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => 'flags-de'
+            ],
+            2 => [
+                'uid' => '2',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'French',
+                'flag' => 'fr',
+                'language_isocode' => 'fr',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => 'flags-fr'
+            ],
+            3 => [
+                'uid' => '3',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'Chinese',
+                'flag' => 'cn',
+                'language_isocode' => 'zh',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => 'flags-cn'
+            ]
+        ];
+
+        static::assertSame($expected, LanguagesHelper::getAll(1));
+    }
+
+    public function testGetAllWithDisabledLanguagesTypoScriptConfig()
+    {
+        $this->setUpBackendUserFromFixture(1);
+
+        $expected = [
+            -1 => [
+                'uid' => '-1',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'All',
+                'flag' => 'multiple',
+                'language_isocode' => 'DEF',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => 'flags-multiple'
+            ],
+            0 => [
+                'uid' => '0',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'Default',
+                'flag' => '',
+                'language_isocode' => 'DEF',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => ''
+            ],
+            1 => [
+                'uid' => '1',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'German',
+                'flag' => 'de',
+                'language_isocode' => 'de',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => 'flags-de'
+            ],
+            3 => [
+                'uid' => '3',
+                'pid' => '0',
+                'tstamp' => '0',
+                'hidden' => '0',
+                'title' => 'Chinese',
+                'flag' => 'cn',
+                'language_isocode' => 'zh',
+                'static_lang_isocode' => '0',
+                'flagIconIdentifier' => 'flags-cn'
+            ]
+        ];
+
+        static::assertSame($expected, LanguagesHelper::getAll(2));
+    }
+
+    public function testGetLanguageIsoCode()
+    {
+        $this->setUpBackendUserFromFixture(1);
+
+        static::assertSame('DEF', LanguagesHelper::getLanguageIsoCode(0, -1));
+        static::assertSame('DEF', LanguagesHelper::getLanguageIsoCode(0, 0));
+        static::assertSame('DE', LanguagesHelper::getLanguageIsoCode(0, 1, true));
+        static::assertSame('FR', LanguagesHelper::getLanguageIsoCode(0, 2, true));
+        static::assertSame('ZH', LanguagesHelper::getLanguageIsoCode(0, 3, true));
+        static::assertSame('', LanguagesHelper::getLanguageIsoCode(0, 4, true)); // non existing language
+    }
+
+    public function testGetLanguageTitle()
+    {
+        $this->setUpBackendUserFromFixture(1);
+
+        static::assertSame('All', LanguagesHelper::getLanguageTitle(0, -1));
+        static::assertSame('Default', LanguagesHelper::getLanguageTitle(0, 0));
+        static::assertSame('German', LanguagesHelper::getLanguageTitle(0, 1));
+        static::assertSame('French', LanguagesHelper::getLanguageTitle(0, 2));
+        static::assertSame('Chinese', LanguagesHelper::getLanguageTitle(0, 3));
+        static::assertSame('Undefined', LanguagesHelper::getLanguageTitle(0, 4)); // non existing language
+    }
+
+    public function testGetLanguageFlagIconIdentifier()
+    {
+        $this->setUpBackendUserFromFixture(1);
+
+        static::assertSame('flags-multiple', LanguagesHelper::getLanguageFlagIconIdentifier(0, -1));
+        static::assertSame('', LanguagesHelper::getLanguageFlagIconIdentifier(0, 0));
+        static::assertSame('flags-de', LanguagesHelper::getLanguageFlagIconIdentifier(0, 1));
+        static::assertSame('flags-fr', LanguagesHelper::getLanguageFlagIconIdentifier(0, 2));
+        static::assertSame('flags-cn', LanguagesHelper::getLanguageFlagIconIdentifier(0, 3));
+        static::assertSame('', LanguagesHelper::getLanguageFlagIconIdentifier(0, 4)); // non existing language
+    }
+
+    public function testGetPageLanguages()
+    {
+        $this->setUpBackendUserFromFixture(1);
+
+        static::assertSame(
+            [
+                0 => [
+                    'uid' => '0',
+                    'pid' => '0',
+                    'tstamp' => '0',
+                    'hidden' => '0',
+                    'title' => 'English',
+                    'flag' => 'gb',
+                    'language_isocode' => 'DEF',
+                    'static_lang_isocode' => '0',
+                    'flagIconIdentifier' => 'flags-gb'
+                ],
+                1 => [
+                    'uid' => '1',
+                    'pid' => '0',
+                    'tstamp' => '0',
+                    'hidden' => '0',
+                    'title' => 'German',
+                    'flag' => 'de',
+                    'language_isocode' => 'de',
+                    'static_lang_isocode' => '0',
+                    'flagIconIdentifier' => 'flags-de'
+                ]
+            ],
+            LanguagesHelper::getPageLanguages(1)
+        );
+        static::assertSame(
+            [
+                0 => [
+                    'uid' => '0',
+                    'pid' => '0',
+                    'tstamp' => '0',
+                    'hidden' => '0',
+                    'title' => 'Default',
+                    'flag' => '',
+                    'language_isocode' => 'DEF',
+                    'static_lang_isocode' => '0',
+                    'flagIconIdentifier' => ''
+                ],
+            ],
+            LanguagesHelper::getPageLanguages(2)
+        );
+    }
+
+    public function testHasPageTranslations()
+    {
+        $this->setUpBackendUserFromFixture(1);
+
+        static::assertTrue(LanguagesHelper::hasPageTranslations(1));
+        static::assertFalse(LanguagesHelper::hasPageTranslations(2));
+    }
+
+    public function testGetNonExistingPageOverlayLanguages()
+    {
+        $this->setUpBackendUserFromFixture(1);
+
+        static::assertSame(
+            [
+                2 => [
+                    'uid' => '2',
+                    'pid' => '0',
+                    'tstamp' => '0',
+                    'hidden' => '0',
+                    'title' => 'French',
+                    'flag' => 'fr',
+                    'language_isocode' => 'fr',
+                    'static_lang_isocode' => '0',
+                    'flagIconIdentifier' => 'flags-fr'
+                ],
+                3 => [
+                    'uid' => '3',
+                    'pid' => '0',
+                    'tstamp' => '0',
+                    'hidden' => '0',
+                    'title' => 'Chinese',
+                    'flag' => 'cn',
+                    'language_isocode' => 'zh',
+                    'static_lang_isocode' => '0',
+                    'flagIconIdentifier' => 'flags-cn'
+                ]
+            ],
+            LanguagesHelper::getNonExistingPageOverlayLanguages(1)
+        );
+
+        static::assertSame(
+            [
+                1 => [
+                    'uid' => '1',
+                    'pid' => '0',
+                    'tstamp' => '0',
+                    'hidden' => '0',
+                    'title' => 'German',
+                    'flag' => 'de',
+                    'language_isocode' => 'de',
+                    'static_lang_isocode' => '0',
+                    'flagIconIdentifier' => 'flags-de'
+                ],
+                3 => [
+                    'uid' => '3',
+                    'pid' => '0',
+                    'tstamp' => '0',
+                    'hidden' => '0',
+                    'title' => 'Chinese',
+                    'flag' => 'cn',
+                    'language_isocode' => 'zh',
+                    'static_lang_isocode' => '0',
+                    'flagIconIdentifier' => 'flags-cn'
+                ]
+            ],
+            LanguagesHelper::getNonExistingPageOverlayLanguages(2)
+        );
+
+        static::assertSame(
+            [
+                1 => [
+                    'uid' => '1',
+                    'pid' => '0',
+                    'tstamp' => '0',
+                    'hidden' => '0',
+                    'title' => 'German',
+                    'flag' => 'de',
+                    'language_isocode' => 'de',
+                    'static_lang_isocode' => '0',
+                    'flagIconIdentifier' => 'flags-de'
+                ],
+                3 => [
+                    'uid' => '3',
+                    'pid' => '0',
+                    'tstamp' => '0',
+                    'hidden' => '0',
+                    'title' => 'Chinese',
+                    'flag' => 'cn',
+                    'language_isocode' => 'zh',
+                    'static_lang_isocode' => '0',
+                    'flagIconIdentifier' => 'flags-cn'
+                ]
+            ],
+            LanguagesHelper::getNonExistingPageOverlayLanguages(3)
+        );
+    }
+}
