85a581c201bf27a814c98fb8701d2d890a8917da
Alexander Schnitzler <git@alexanderschnitzler.de>
2017-03-20 09:12:58 +0100

    [SECURITY] Remove possibility to access arbitrary session data

--- Configuration/Backend/AjaxRoutes.php
+++ Configuration/Backend/AjaxRoutes.php
@@ -12,11 +12,6 @@
  */
 
 return [
-    // Expand or toggle in legacy database tree
-    'TemplaVoila::AdministrationModule::getFileContent' => [
-        'path' => '/templavoila/admininstration/ajax',
-        'target' => \Schnitzler\Templavoila\Controller\Backend\AdministrationModule\AjaxController::class . '::getFileContent'
-    ],
     'TemplaVoila::Api::Unlink' => [
         'path' => '/templavoila/api/unlink',
         'target' => \Schnitzler\Templavoila\Controller\Backend\Ajax\ApiController::class. '::unlink'
